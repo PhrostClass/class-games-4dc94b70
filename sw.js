e0ea4d52efb17e3661170060285308508e55ff01
fepsi <fepsi92@gmail.com>
2026-09-16 23:58:34 +0200

    v1.1.1 - Tilt controls for charades (forehead: tilt down = got it, tilt back = pass)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Class Games service worker. Bump VERSION on every deploy (deploy.ps1 does it). */
-const VERSION = '1.1.0';
+const VERSION = '1.1.1';
 const CACHE = 'classgames-' + VERSION;
 const SHELL = ['./', './index.html', './manifest.webmanifest',
   './icons/icon-192.png', './icons/icon-512.png', './icons/apple-touch-icon.png'];
